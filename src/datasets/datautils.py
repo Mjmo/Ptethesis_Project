33e490844a8f7cc6d2c3be7584e155624d2db439
Ahmad HAwa <lesmatrix100@gmail.com>
2026-01-04 20:42:22 +0100

    Updating datautils

--- src/datasets/datautils.py
+++ src/datasets/datautils.py
@@ -4,8 +4,10 @@ from sklearn.model_selection import StratifiedShuffleSplit
 from torch.utils.data import Subset,Dataset
 from collections import Counter
 import random
+from PIL import Image
 from torchvision.transforms import ToPILImage
 from typing import Tuple,List
+random.seed(42)
 def get_and_split(path:str, testsize:float=0.2)->Tuple[Subset,Subset]:
     dataset = ImageFolder(path, transform=None)
 
@@ -22,35 +24,6 @@ def get_and_split(path:str, testsize:float=0.2)->Tuple[Subset,Subset]:
     val_data   = Subset(dataset, val_idx)
 
     return train_data, val_data
-def get_minority_classes(targets:List[int], threshold:int=50)->List[int]:
-    count = Counter(targets)
-    return [cls for cls, n in count.items() if n < threshold]
-class OversampledAugmentedDataset(Dataset):from torchvision.datasets import ImageFolder
-import numpy as np 
-from sklearn.model_selection import StratifiedShuffleSplit
-from torch.utils.data import Subset,Dataset
-from collections import Counter
-import random
-from typing import Tuple,List
-def get_and_split(path:str, testsize:float=0.2)->Tuple[Subset,Subset]:
-    dataset = ImageFolder(path, transform=None)
-
-    targets = [label for _, label in dataset.samples]
-    indices = np.arange(len(targets))
-    targets_np = np.array(targets)
-
-    splitter = StratifiedShuffleSplit(
-        n_splits=1, test_size=testsize, random_state=42
-    )
-    train_idx, val_idx = next(splitter.split(indices, targets_np))
-
-    train_data = Subset(dataset, train_idx)
-    val_data   = Subset(dataset, val_idx)
-
-    return train_data, val_data
-def get_minority_classes(targets:List[int], threshold:int=50)->List[int]:
-    count = Counter(targets)
-    return [cls for cls, n in count.items() if n < threshold]
 class OversampledAugmentedDataset(Dataset):
     def __init__(self, base_dataset, min_samples, minority_classes, augmentations):
         self.base_dataset = base_dataset
@@ -79,8 +52,14 @@ class OversampledAugmentedDataset(Dataset):
         return len(self.indices)
 
     def __getitem__(self, idx):
-        topil=ToPILImage()
-        img, label = self.base_dataset.dataset[self.indices[idx]]
-        img=topil(img)
+        if isinstance(self.base_dataset, Subset):
+            img, label = self.base_dataset.dataset[self.indices[idx]]
+        else:
+            img, label = self.base_dataset[self.indices[idx]]
+
+    # Convert to PIL if not already
+        if not isinstance(img, Image.Image):
+            img = ToPILImage()(img)
+
         img = self.augmentations(img)
         return img, label
