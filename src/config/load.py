78426dcdfe57fec385e53504980d592699b7071b
Ahmad HAwa <lesmatrix100@gmail.com>
2026-02-07 14:41:53 +0100

    Editing config

--- src/config/load.py
+++ src/config/load.py
@@ -7,7 +7,7 @@ import yaml
 def load_config(path:str)->ExpirementConfig:
     with open(path,"r") as file:
         raw=yaml.safe_load(file)
-    return ExpirementConfig(seed=raw["seed"],data=DataConfig(**raw["model"]),model=ModelConfig(**raw["model"]),trainig=Training_Config(**raw["training"]))
+    return ExpirementConfig(seed=raw["seed"],data=DataConfig(**raw["data"]),model=ModelConfig(**raw["model"]),trainig=Training_Config(**raw["training"]))
 
 
 
