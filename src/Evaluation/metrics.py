2d739ae54ebb573943f71995c71f328adbf2ca26
mjmo <lesmatrix100@gmail.com>
2026-02-24 21:57:38 +0100

    Added comparison

--- src/Evaluation/metrics.py
+++ src/Evaluation/metrics.py
@@ -138,8 +138,8 @@ def plot_multiclass_pr(y_true, y_probs, class_names=None):
     plt.tight_layout()
     plt.show(  )
     
-def plot_comparison_matrix(cm,y_true,y_preds,true_names,pred_names,figure_size=(20,20)):
-        cm=confusion_matrix(y_true,y_preds,normalize=True)
+def plot_comparison_matrix(y_true,y_preds,true_names,pred_names,figure_size=(20,20)):
+        cm=confusion_matrix(y_true,y_preds,normalize="true")
         plt.figure(figsize=figure_size)
         sns.heatmap(cm,annot=True,xticklabels=pred_names,yticklabels=true_names)
         plt.xlabel("True labels")
