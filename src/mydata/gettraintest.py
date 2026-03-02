4c40ebd6e2fc71b499c4146784c512587e28f47f
mjmo <lesmatrix100@gmail.com>
2026-03-02 21:23:00 +0100

    Editing Data utils and model Functions

--- src/mydata/gettraintest.py
+++ src/mydata/gettraintest.py
@@ -15,7 +15,7 @@ def get_data_loader(
     seed:int,
     test_size:float=0.1,
 )->Tuple[DataLoader,DataLoader]:
-    trainset, valset = get_and_split(dataset, test_size)
+    trainset, valset = get_and_split(dataset, test_size,seed=seed)
 
     train_targets = [trainset.dataset.targets[i] for i in trainset.indices]
 
